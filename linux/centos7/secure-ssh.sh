601b22484087f358c5981a22b4af01661dd99528
BitterCandies <starblaze647@gmail.com>
2020-02-20 16:43:17 +0000

    updated secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,9 +4,9 @@
 #adds a public key from the local repo or curled from the remote repo
 #removes all roots ability to ssh in
 echo "ALL YOUR CODE GOES HERE"
-sudo useradd -m -d /home/taossh -s /bin/bash taossh
-sudo mkdir /home/taossh/.ssh
-sudo cp /home/tao/SYS265_S20/linux/public-keys/id_rsa.pub /home/taossh/.ssh/authorized_keys
-sudo chmod 700 /home/taossh/.ssh
-sudo chmod 600 /home/taossh/.ssh/authorized_keys
-sudo chown -R taossh:taossh /home/taossh/.ssh
+sudo useradd -m -d /home/$1 -s /bin/bash $1
+sudo mkdir /home/$1/.ssh
+sudo cp /home/tao/SYS265_S20/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
